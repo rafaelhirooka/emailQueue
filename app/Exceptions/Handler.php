71f0927c08c45121c7c78bfcbb85680e6626cbdf
rafael.hirooka <rafaelhirooka@gmail.com>
2019-09-27 17:13:37 -0300

    exceptions handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,22 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Exception;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -46,6 +57,41 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if (env('APP_DEBUG', false))
+            return parent::render($request, $exception);
+
+        if ($exception instanceof HttpException) {
+            $code = $exception->getStatusCode();
+            $message = $exception->getMessage() == NULL ? Response::$statusTexts[$code] : $exception->getMessage();
+            return $this->errorResponse($message, $code);
+        }
+        if ($exception instanceof QueryException) {
+            $code = $exception->getCode();
+            return $this->errorResponse('Erro de consulta. Código: ' . $code, Response::HTTP_BAD_REQUEST);
+        }
+        if ($exception instanceof ModelNotFoundException) {
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse('Não existe nenhuma instância de ' . $model . ' com a referência passada', Response::HTTP_NOT_FOUND);
+        }
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+        if ($exception instanceof AuthenticationException) {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+        if ($exception instanceof ClientException) {
+            $message = $exception->getResponse()->getBody();
+            $code = $exception->getCode();
+            return $this->errorResponse($message, $code);
+        }
+        if ($exception instanceof ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        if ($exception instanceof \RuntimeException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getCode());
+        }
+
+        return $this->errorResponse('Erro inesperado. Tente mais tarde.', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
