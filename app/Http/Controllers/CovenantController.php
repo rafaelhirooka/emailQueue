bec61ae6444837b8eb8469a7ae8011ba3034805b
rafael.hirooka <rafaelhirooka@gmail.com>
2019-09-30 16:08:36 -0300

    added job trait on covenant controller

--- app/Http/Controllers/CovenantController.php
+++ app/Http/Controllers/CovenantController.php
@@ -6,13 +6,12 @@ use App\Jobs\SendEmailJob;
 use App\Mail\Covenant\Register;
 use App\Mail\Covenant\RegisterMS;
 use App\Traits\ApiResponser;
+use App\Traits\EmailJob;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Validator;
 
 class CovenantController extends Controller {
 
-    use ApiResponser;
+    use ApiResponser, EmailJob;
 
     private $rules = [
         'subject' => 'required',
@@ -21,30 +20,18 @@ class CovenantController extends Controller {
     ];
 
     public function register(Request $request) {
-        $data = $request->all();
+        $data = $this->prepare($request->all());
 
-        $v = Validator::make($data, $this->rules);
-        if ($v->fails())
-            throw new \RuntimeException('Verifique se não esqueceu nenhum parâmetro obrigatório', Response::HTTP_UNPROCESSABLE_ENTITY);
-
-        $data['to'] = is_string($data['to']) ? (array)json_decode($data['to']) : (array)$data['to'];
-        $data['data'] = is_string($data['data']) ? (array)json_decode($data['data']) : (array)$data['data'];
-        dispatch(new SendEmailJob(new Register($data['subject'], $data['data']), $data['to']));
-
-        return $this->successResponse('queue');
+        return $this->queue(new SendEmailJob(new Register($data['subject'], $data['data']), $data['to']), function() {
+            return $this->successResponse('queue');
+        });
     }
 
     public function registerMS(Request $request) {
-        $data = $request->all();
+        $data = $this->prepare($request->all());
 
-        $v = Validator::make($data, $this->rules);
-        if ($v->fails())
-            throw new \RuntimeException('Verifique se não esqueceu nenhum parâmetro obrigatório', Response::HTTP_UNPROCESSABLE_ENTITY);
-
-        $data['to'] = is_string($data['to']) ? (array)json_decode($data['to']) : (array)$data['to'];
-        $data['data'] = is_string($data['data']) ? (array)json_decode($data['data']) : (array)$data['data'];
-        dispatch(new SendEmailJob(new RegisterMS($data['subject'], $data['data']), $data['to']));
-
-        return $this->successResponse('queue');
+        return $this->queue(new SendEmailJob(new RegisterMS($data['subject'], $data['data']), $data['to']), function() {
+            return $this->successResponse('queue');
+        });
     }
 }
